04bacb7d4473b34d04e90cd35b241167e3bc726c
Dante Archer <dantearcher@delta.edu>
2026-05-16 02:35:01 -0400

    Finish escalation logic

--- src/core/escalation.ts
+++ src/core/escalation.ts
@@ -1,3 +1,5 @@
+import { reddit } from "@devvit/web/server";
+
 export async function checkEscalation(
     activeCount: number,
     currentTier: number,
@@ -15,7 +17,30 @@ export async function checkEscalation(
 
 
     if (newTier > currentTier) {
-        console.log(`Escalating ${userName} from Tier ${currentTier} to Tier ${newTier}`)
+        if (newTier === 1) {
+            await reddit.sendPrivateMessage({
+                to: userName,
+                subject: `Warning from r/${subredditName}`,
+                text: 'This is a warning'
+            })
+        } else if (newTier === 2) {
+            await reddit.banUser({
+                username: userName,
+                subredditName: subredditName,
+                context: 'Temp Ban',
+                message: `You are temperarily banned from r/${subredditName}`,
+                reason: 'Hate speech',
+                duration: 14,
+            })
+        } else if (newTier === 3) {
+            await reddit.banUser({
+                username: userName,
+                subredditName: subredditName,
+                context: 'Permanent Ban',
+                message: `You are permanently banned from r/${subredditName}`,
+                reason: 'Repeated violations',
+            })
+        }
     }
 
     return newTier;
