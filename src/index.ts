9ff021a0a622780e716f9456f2f8d898c641b94b
Dante Archer <dantearcher@delta.edu>
2026-05-22 08:00:26 -0400

    Remove api from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
 import { createServer, getServerPort } from '@devvit/web/server';
-import { api } from './routes/api';
 import { forms } from './routes/forms';
 import { menu } from './routes/menu';
 import { triggers } from './routes/triggers';
@@ -15,7 +14,6 @@ internal.route('/form', forms);
 internal.route('/triggers', triggers);
 internal.route('/scheduler', scheduler)
 
-app.route('/api', api);
 app.route('/internal', internal);
 
 serve({
